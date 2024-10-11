60a37ba68a0c538a87526c8cccfd93f32ab205b7
Abdur Razzak <abdur.razzak@silverlakeaxis.com>
2024-10-11 17:29:46 +0800

    Implement Swagger UI and spring security basic authentication

--- src/main/java/com/mir00r/practiceApacheCamel/domains/edakafka/controllers/UserActivityController.java
+++ src/main/java/com/mir00r/practiceApacheCamel/domains/edakafka/controllers/UserActivityController.java
@@ -2,7 +2,7 @@ package com.mir00r.practiceApacheCamel.domains.edakafka.controllers;
 
 import com.mir00r.practiceApacheCamel.domains.edakafka.models.UserSearchRequest;
 import com.mir00r.practiceApacheCamel.domains.edakafka.models.UserSignUpRequest;
-import com.mir00r.practiceApacheCamel.domains.edakafka.routes.UserActivityKafkaRoute;
+//import com.mir00r.practiceApacheCamel.domains.edakafka.routes.UserActivityKafkaRoute;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +25,7 @@ public class UserActivityController {
   @PostMapping("/v1/users/signup")
   public ResponseEntity<String> signUpUser(@RequestBody UserSignUpRequest request) {
     // Publish the user signup event to the Kafka topic
-    this.producerTemplate.sendBody(UserActivityKafkaRoute.USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS,
+    this.producerTemplate.sendBody("UserActivityKafkaRoute.USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS",
       request);
     return ResponseEntity.ok("User sign-up event published successfully!");
   }
@@ -33,7 +33,7 @@ public class UserActivityController {
   @PostMapping("/v1/users/search")
   public ResponseEntity<String> userSearch(@RequestBody UserSearchRequest request) {
     // Publish the user search event to the Kafka topic
-    this.producerTemplate.sendBody(UserActivityKafkaRoute.USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS,
+    this.producerTemplate.sendBody("UserActivityKafkaRoute.USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS",
       request);
     return ResponseEntity.ok("User search event published successfully!");
   }
--- src/main/java/com/mir00r/practiceApacheCamel/domains/edakafka/routes/UserActivityKafkaRoute.java
+++ src/main/java/com/mir00r/practiceApacheCamel/domains/edakafka/routes/UserActivityKafkaRoute.java
@@ -4,45 +4,45 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
-public class UserActivityKafkaRoute extends RouteBuilder {
-
-  @Value("${spring.kafka.consumer.group-id}")
-  private static String groupId;
-
-  @Value("${camel.component.kafka.brokers}")
-  private static String brokerUrl;
-
-  public static final String USER_SIGNUP_KAFKA_TOPIC = "kafka:topic-user-signup";
-  public static final String USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS =
-    String.format("%s?brokers=%s", USER_SIGNUP_KAFKA_TOPIC, brokerUrl);
-  public static final String USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID =
-    String.format("%s&groupId=%s", USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS, groupId);
-
-  public static final String USER_SEARCH_KAFKA_TOPIC = "kafka:topic-user-search";
-  public static final String USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS =
-    String.format("%s?brokers=%s", USER_SEARCH_KAFKA_TOPIC, brokerUrl);
-  public static final String USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID =
-    String.format("%s&groupId=%s", USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS, groupId);
-
-  @Override
-  public void configure() throws Exception {
-    // Route for consuming user sign-up events from Kafka and triggering further processing
-    from(USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID)
-      .log("Received user sign-up event: ${body}")
-      .process(exchange -> {
-        // Custom logic for processing sign-up events, e.g., sending a welcome email
-        String userSignupData = exchange.getIn().getBody(String.class);
-        System.out.println("Processing sign-up event: " + userSignupData);
-      });
-
-    // Route for consuming user search events from Kafka
-    from(USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID)
-      .log("Received user search event: ${body}")
-      .process(exchange -> {
-        // Custom logic for processing search events
-        String userSearchData = exchange.getIn().getBody(String.class);
-        System.out.println("Processing search event: " + userSearchData);
-      });
-  }
-}
+//@Component
+//public class UserActivityKafkaRoute extends RouteBuilder {
+//
+//  @Value("${spring.kafka.consumer.group-id}")
+//  private static String groupId;
+//
+//  @Value("${camel.component.kafka.brokers}")
+//  private static String brokerUrl;
+//
+//  public static final String USER_SIGNUP_KAFKA_TOPIC = "kafka:topic-user-signup";
+//  public static final String USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS =
+//    String.format("%s?brokers=%s", USER_SIGNUP_KAFKA_TOPIC, brokerUrl);
+//  public static final String USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID =
+//    String.format("%s&groupId=%s", USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS, groupId);
+//
+//  public static final String USER_SEARCH_KAFKA_TOPIC = "kafka:topic-user-search";
+//  public static final String USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS =
+//    String.format("%s?brokers=%s", USER_SEARCH_KAFKA_TOPIC, brokerUrl);
+//  public static final String USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID =
+//    String.format("%s&groupId=%s", USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS, groupId);
+//
+//  @Override
+//  public void configure() throws Exception {
+//    // Route for consuming user sign-up events from Kafka and triggering further processing
+//    from(USER_SIGNUP_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID)
+//      .log("Received user sign-up event: ${body}")
+//      .process(exchange -> {
+//        // Custom logic for processing sign-up events, e.g., sending a welcome email
+//        String userSignupData = exchange.getIn().getBody(String.class);
+//        System.out.println("Processing sign-up event: " + userSignupData);
+//      });
+//
+//    // Route for consuming user search events from Kafka
+//    from(USER_SEARCH_KAFKA_TOPIC_WITH_BROKERS_WITH_GROUP_ID)
+//      .log("Received user search event: ${body}")
+//      .process(exchange -> {
+//        // Custom logic for processing search events
+//        String userSearchData = exchange.getIn().getBody(String.class);
+//        System.out.println("Processing search event: " + userSearchData);
+//      });
+//  }
+//}
